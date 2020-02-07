af3e164b4d013638b54a846ed5dc0f23405797bf
jakubG78 <jakub.gora@wp.pl>
2020-02-07 23:02:19 +0100

    Commit for v0.1

--- src/com/kodilla/BlackJack.java
+++ src/com/kodilla/BlackJack.java
@@ -1,5 +1,31 @@
 package com.kodilla;
 
-public class BlackJack {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+public class BlackJack extends Application {
+
+    private Image imageback = new Image("file:resources/table.png");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
