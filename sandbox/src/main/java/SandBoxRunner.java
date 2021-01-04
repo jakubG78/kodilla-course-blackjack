c879622e64295d820f931dba0d6b3c856828423e
jakubG78 <jakub.gora@wp.pl>
2021-01-04 22:20:14 +0100

    backup commit 01.01.2021

---
+++ sandbox/src/main/java/SandBoxRunner.java
@@ -0,0 +1,9 @@
+public class SandBoxRunner {
+    public static void main(String[] args) {
+        String name ="WasdweWrfefAREDDsdvcvsXd";
+        String lowName = name.toLowerCase();
+        System.out.println(lowName);
+        long counter = lowName.chars().count();
+        System.out.println(counter);
+    }
+}
